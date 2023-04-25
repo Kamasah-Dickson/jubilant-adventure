723ef7612eafb653623c1e6e27c0c7cad37875ba
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-25 02:42:22 +0000

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+	let year = new Date().getFullYear();
+	return (
+		<footer>
+			<p
+				style={{
+					fontSize: "14px",
+				}}
+			>
+				Created by Kamasah Dickson with 💖
+			</p>
+			<div></div>
+			<div>
+				<p>&copy; {year} All rights reserved</p>
+			</div>
+		</footer>
+	);
+}
+
+export default Footer;
