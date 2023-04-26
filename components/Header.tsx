bc3885b242ec31383fc11cf5e7e8f2e23a8a91da
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-26 11:27:18 +0000

    refactor Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,40 @@
 import { globalContext } from "@/context/appContext";
-import { useContext } from "react";
+import { useContext, Dispatch, useEffect, SetStateAction } from "react";
 import { RiMenu4Line } from "react-icons/ri";
 import sideBarStyles from "../styles/sidebar.module.scss";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
 
 type Ititle = {
 	title: string;
 };
+
 function Header({ title }: Ititle) {
-	const { setShowNav } = useContext(globalContext);
+	const { setShowNav, setDarkmode, darkmode } = useContext(globalContext);
+
+	useEffect(() => {
+		if (darkmode) {
+			document.body.classList.remove("light-mode");
+			document.body.classList.add("dark-mode");
+		} else {
+			document.body.classList.remove("dark-mode");
+			document.body.classList.add("light-mode");
+		}
+	}, [darkmode]);
+
 	return (
-		<div
-			onClick={() => setShowNav(true)}
-			className={sideBarStyles.hamburgerAndtitle}
-		>
-			<div className={sideBarStyles.hamburger}>
+		<div className={sideBarStyles.hamburgerAndtitle}>
+			<div onClick={() => setShowNav(true)} className={sideBarStyles.hamburger}>
 				<RiMenu4Line size={25} />
 			</div>
-			<h1>{title}</h1>
+			<div className={sideBarStyles.titleFlex}>
+				<h1>{title}</h1>
+				<div
+					onClick={() => setDarkmode((prev) => !prev)}
+					className={sideBarStyles.icon}
+				>
+					{darkmode ? <MdOutlineLightMode color="white" /> : <MdDarkMode />}
+				</div>
+			</div>
 		</div>
 	);
 }
