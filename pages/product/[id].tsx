cd6e641d3323f8e0c1280307be5bfe7cdbc7c70c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-04-26 21:58:13 +0000

    update singleProduct

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -10,7 +10,7 @@ import { CartType } from "@/context/globlaTypes";
 
 function SingleProductPage() {
 	const { query } = useRouter();
-	const { productCounts, addItemToCart } = useContext(globalContext);
+	const { productCounts, addItemToCart, products } = useContext(globalContext);
 	const count = productCounts[String(query.id)] || 0;
 
 	let thisProductDetails: CartType = {
@@ -18,7 +18,10 @@ function SingleProductPage() {
 		image: query.image,
 		price: String(query.price),
 		title: String(query.title),
+		description: String(query.description),
 	};
+
+	let index = products.findIndex((product) => product.id == query.id);
 
 	return (
 		<>
@@ -35,7 +38,9 @@ function SingleProductPage() {
 					<div className={styles.singleCard}>
 						<div className={styles.cardImage}>
 							<Image
-								src={String(query.image)}
+								// src={thisProductDetails.image}
+								src={products[index].image}
+								priority
 								width={350}
 								height={350}
 								alt=""
@@ -43,11 +48,13 @@ function SingleProductPage() {
 						</div>
 						<div className={styles.productDescriptions}>
 							<div>
-								<p className={styles.title}>Macbook pro 16</p>
-								<p className={styles.subtitle}>Silve-M1 Pro</p>
+								<p className={styles.title}>
+									{thisProductDetails.title.slice(0, 35) + "..."}
+								</p>
+								<p className={styles.subtitle}></p>
 							</div>
 							<div className={styles.price}>
-								<p>{query.price}</p>
+								<p>${query.price}</p>
 								<div
 									onClick={() => addItemToCart(thisProductDetails)}
 									className={styles.addTocart}
@@ -59,12 +66,7 @@ function SingleProductPage() {
 						</div>
 					</div>
 					<div className={styles.description}>
-						<p>
-							Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic
-							minima natus magni, perferendis totam et nulla id cupiditate
-							eligendi quia aliquid, assumenda reprehenderit. Nulla dolore quos
-							earum sunt sequi molestiae!
-						</p>
+						<p>{thisProductDetails.description}</p>
 					</div>
 				</div>
 				<Footer />
